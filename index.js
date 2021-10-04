6fed28f78c0d60643e5ef7c8b4c76b8a762726fb
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2021-10-03 22:35:46 -0500

    add index.js, connect to index.html

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let h2 = document.createElement("h2");
+const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
-document.querySelector("boby").appendChild(h2);
+document.querySelector("body").appendChild(h2);
