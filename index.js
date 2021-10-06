eff3a368200df30604cae52b47c4e02086e3175f
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2021-10-06 18:45:21 -0500

    editing in progress

--- index.js
+++ index.js
@@ -2,39 +2,48 @@
 const init = () => {
     let inputForm = document.querySelector('form');
 
-    inputForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        console.log(event);
-
-        fetch('http://localhost:3000/workouts')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        });
-
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let exerciseObj = {
+            name: e.target.exercise_name.value,
+            sets: e.target.sets.value,
+            reps: e.target.reps.value
+        };
+        renderOneExercise(exerciseObj);
+        addExercise(exerciseObj);
+         console.log(e);
+         console.log(exerciseObj);
     });
 }
 
+//Event Listeners
 document.addEventListener('DOMContentLoaded', init)
+
+//Event Handlers
+function handleSubmit(e){
+}
 
 //DOM Render Workout
 function renderOneExercise(exercise){
     //Build Exercise Card
-    let card = document.createElement('li');
+    let card = document.createElement('div');
     card.className = 'card'
     card.innerHTML = `
-        <div class="content">
-            <h2>${exercise.name}</h2>
-            <p>
-                $<span class="sets-reps">${exercise.sets} x ${exercise.reps}
-            </p>
-        </div>
-        <div class="button">
-            <button class="completed-btn" id="${exercise.name}> Completed! </button>
-        </div
+    <div class="content">
+    <h3>${exercise.name}</h3>
+    <p>
+    <span class="sets-reps">${exercise.sets} sets x ${exercise.reps} reps</span>
+    </p>
+    <div class="button">
+    <button class="completed-btn" id="${exercise.name}"> Completed </button>
+    </div>
+    </div>
     `
     //Add exercise card to DOM
     document.querySelector('#exercise-list').appendChild(card);
+    document.getElementById(exercise.name).addEventListener('click', () => {
+        card.remove()
+    })
 };
 
 //Fetch Request
@@ -46,14 +55,15 @@ function getAllExercises(){
 
 
 //Adding exercises to JSON server data
-function addExercise(){
-    fetch('http://localhost:3000/workouts'),{
-        method: 'POST' ,
-        headers:
-        {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-        },
+function addExercise(exerciseObj){
+    console.log(JSON.stringify(exerciseObj))
+    // fetch('http://localhost:3000/workouts'),{
+    //     method: 'POST' ,
+    //     headers:
+    //     {
+    //         "Content-Type": "application/json",
+    //         Accept: "application/json"
+    //     },
 
-    }
+    // }
 }
